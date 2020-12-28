1ffd72cdd910bf9e1b02cdddd49adfbbd980f02a
monica-0218 <sorakawa0218@gmail.com>
2020-12-28 20:37:13 +0900

    add static settings and ignore __pycache__.py file

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,10 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 SUMMERNOTE_THEME = 'bs4'
 X_FRAME_OPTOPNS = 'SAMEORIGIN'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
